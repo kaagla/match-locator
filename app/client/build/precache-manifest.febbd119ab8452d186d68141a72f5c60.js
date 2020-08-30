23fe83570ee40c67ad215d4e5749a946bc067b03
Kaarle Glad <kaarle.glad@student.tut.fi>
2020-08-30 15:34:49 +0300

    Frontend, react build folder

---
+++ app/client/build/precache-manifest.febbd119ab8452d186d68141a72f5c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf1ae77cc4193c42d78d19b19be4aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bb54252f3ec78bcdf9c",
+    "url": "/static/css/main.45dfaa79.chunk.css"
+  },
+  {
+    "revision": "117768d6bb4dcbdb2f8b",
+    "url": "/static/js/2.49a6b8f5.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.49a6b8f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb54252f3ec78bcdf9c",
+    "url": "/static/js/main.b5caf341.chunk.js"
+  },
+  {
+    "revision": "a50eecbd7126ef8cca64",
+    "url": "/static/js/runtime-main.a27e5ec6.js"
+  }
+]);
